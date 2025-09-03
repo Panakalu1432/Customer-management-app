37226131766d28d2c18d6fd9fb695e9664fcb94a
Panakalu1432 <109236235+Panakalu1432@users.noreply.github.com>
2025-09-03 07:32:01 +0530

    Update customers.js

--- server/routes/customers.js
+++ server/routes/customers.js
@@ -6,7 +6,6 @@ const customerController = require('../controllers/customerController');
 router.post('/', customerController.createCustomer);
 router.get('/', customerController.getCustomers);
 router.get('/:id', customerController.getCustomerById);
-router.put('/:id', customerController.updateCustomer);
 router.delete('/:id', customerController.deleteCustomer);
 
 module.exports = router;
